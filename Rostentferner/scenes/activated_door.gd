d7b598720e22a010e628ec92c0b54020c343135c
CelerityTAS <122490724+CelerityTAS@users.noreply.github.com>
2024-04-20 18:22:39 +0200

    door color

--- Rostentferner/scenes/activated_door.gd
+++ Rostentferner/scenes/activated_door.gd
@@ -6,6 +6,7 @@ extends StaticBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	button.ButtonPressed.connect(_on_button_button_pressed)
+	$AnimatedSprite2D.material.set_shader_parameter("activated",false)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,5 +15,6 @@ func _process(delta):
 
 func _on_button_button_pressed():
 	$AnimatedSprite2D.play("default")
+	$AnimatedSprite2D.material.set_shader_parameter("activated",true)
 	self.collision_layer=8
 	open = !open
