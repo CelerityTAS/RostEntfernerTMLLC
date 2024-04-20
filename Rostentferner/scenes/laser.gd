f81a39b7a1e07f1f08c6ea7a8ac74cc2d2f93a61
Gunihelm <lennart@zurmuehls.de>
2024-04-20 12:57:52 +0200

    added laser and controll node

--- Rostentferner/scenes/laser.gd
+++ Rostentferner/scenes/laser.gd
@@ -1,18 +1,35 @@
 extends Area2D
-@onready var colission = $Collision
 var trackedPos = []
 var lastTrackedPos = []
+var step = 0
+@export var baseSpeed = 30
 @onready var player = $"../player"
+@onready var timer = $Timer
+var nextMove = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+func reset():
+	lastTrackedPos = trackedPos
+	position.x = -598
+	step = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position.x += nextMove*4*delta
+	
+	
+	for body in get_overlapping_bodies():
+		if body.has_function("die"):
+			body.die()
 
 
 func _on_timer_timeout():
-	trackedPos.append(player.poaition.x)
+	trackedPos.append(player.position.x)
+	step += 1
+	if step<lastTrackedPos.size():
+		nextMove = lastTrackedPos[step]-position.x
+	else:
+		nextMove = baseSpeed
+	
