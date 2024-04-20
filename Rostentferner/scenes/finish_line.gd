3584dd4ac52d2298ce2b44cb5cf0a0456b03de3e
CelerityTAS <122490724+CelerityTAS@users.noreply.github.com>
2024-04-20 19:58:19 +0200

    RunCounter und so

--- Rostentferner/scenes/finish_line.gd
+++ Rostentferner/scenes/finish_line.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 
 var deaths = 0
+var times_completed = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	deaths=0
+	times_completed=0
 
 func resetdeaths():
 	deaths=0
@@ -23,4 +25,7 @@ func _on_area_2d_body_entered(body):
 	$"../player".reset()
 	get_parent().get_node("Control").paused=true
 	resetdeaths()
+	times_completed+=1
+	
+	$"../HUD/RunCounter/RunCount".text=str(times_completed)
 	pass # Replace with function body.
