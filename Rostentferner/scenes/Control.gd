926fde2b6cc0c9ebfa579948523b4db5c9af2120
Gunihelm <lennart@zurmuehls.de>
2024-04-20 13:50:34 +0200

    fixed reset and laser

--- Rostentferner/scenes/Control.gd
+++ Rostentferner/scenes/Control.gd
@@ -1,6 +1,7 @@
 extends Control
 
-
+@onready var player = $"../player"
+var paused = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,5 +11,11 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+	
+	if paused and abs(player.movement.x) > 5:
+		$"../Laser".start()
+		paused = false
 	if Input.is_action_just_pressed("ui_accept"):
 		$"../Laser".reset()
+		player.reset()
+		paused = true
--- Rostentferner/scenes/laser.gd
+++ Rostentferner/scenes/laser.gd
@@ -1,20 +1,26 @@
 extends Area2D
 var trackedPos = []
 var lastTrackedPos = []
-var step = 0
+var step = -1
 @export var baseSpeed = 30
 @onready var player = $"../player"
 @onready var timer = $Timer
 var nextMove = 0
+@export var DELAY = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func start():
+	step = 0
+
 func reset():
 	lastTrackedPos = trackedPos
+	trackedPos = []
 	position.x = -598
-	step = 0
+	nextMove = 0
+	step = -1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x += nextMove*4*delta
@@ -26,10 +32,12 @@ func _process(delta):
 
 
 func _on_timer_timeout():
-	trackedPos.append(player.position.x)
-	step += 1
-	if step<lastTrackedPos.size():
-		nextMove = lastTrackedPos[step]-position.x
-	else:
-		nextMove = baseSpeed
+	if step != -1:
+		trackedPos.append(player.position.x)
+		step += 1
+		if step-DELAY>=0:
+			if step-DELAY<lastTrackedPos.size():
+				nextMove = lastTrackedPos[step-DELAY]-position.x
+			else :
+				nextMove = baseSpeed
 	
