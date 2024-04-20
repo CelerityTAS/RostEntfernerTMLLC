b020bd4bda89aeebc5c90c809f662990e64e2346
Gunihelm <lennart@zurmuehls.de>
2024-04-20 11:28:25 +0200

    short jump + drag example

--- Rostentferner/scenes/Player/player.gd
+++ Rostentferner/scenes/Player/player.gd
@@ -4,11 +4,11 @@ extends CharacterBody2D
 
 const UP_VECTOR = Vector2(0,-1)
 
-@export var SPEED = 100.0
+@export var SPEED = 50
 @export var JUMP_VELOCITY = -400.0
 @export var MAX_JUMP_TIMER = 11
 @export var MAX_COYOTE_TIMER = 0.1 #in Sec
-@export var DRAG = 0.8
+@export var DRAG = 0.9
 @export var GRAVITY = 1000
 @onready var sprite = $AnimatedSprite2D
 var jump_available = false
@@ -19,6 +19,8 @@ func _physics_process(delta):
 	if Input.is_action_pressed("jump") and jump_available:
 		movement.y = JUMP_VELOCITY
 		jump_available = false
+	if !Input.is_action_pressed("jump") and movement.y < 0:
+		movement.y *= 0.7
 	
 	set_velocity(movement)
 	set_up_direction(UP_VECTOR)
