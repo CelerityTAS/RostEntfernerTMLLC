8fa83b67d5b0324c3009422ef7a1cd8106de275d
Gunihelm <lennart@zurmuehls.de>
2024-04-21 15:39:45 +0200

    stable with bug

--- Rostentferner/scenes/Player/player.gd
+++ Rostentferner/scenes/Player/player.gd
@@ -15,6 +15,7 @@ var isAlive = true
 var jump_available = false
 var movement = Vector2()
 
+
 func _physics_process(_delta):
 	if (!isAlive or $AnimatedSprite2D.animation=="die"): return
 	if Input.is_action_just_pressed("jump"):
