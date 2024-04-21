23eee44d5ad6615285c51a61646f5a0f76f5631b
Gunihelm <lennart@zurmuehls.de>
2024-04-21 12:30:46 +0200

    NO MORE FORGIVING DEATH LASER

--- Rostentferner/scenes/laser.gd
+++ Rostentferner/scenes/laser.gd
@@ -2,11 +2,14 @@ extends Area2D
 var trackedPos = []
 var lastTrackedPos = []
 var step = -1
+var replayStep = 0
 @export var baseSpeed = 30
 @onready var player = $"../player"
 @onready var timer = $Timer
+@onready var replay_timer = $replayTimer
+
 var nextMove = 0
-@export var DELAY = 2
+@export var delay = 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,14 +17,18 @@ func _ready():
 
 func start():
 	step = 0
+	replayStep = 0
 
 func reset(hasFinished):
 	if hasFinished:
 		lastTrackedPos = trackedPos
+		replay_timer.wait_time = 0.25*(lastTrackedPos.size() )/(lastTrackedPos.size()+ delay*1.0)
 	trackedPos = []
 	position.x = -598
 	nextMove = 0
 	step = -1
+	replayStep = 0
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -37,13 +44,17 @@ func _process(delta):
 func _on_timer_timeout():
 	if step != -1:
 		trackedPos.append(player.position.x)
-		if round(lastTrackedPos.size()/8)>0:
-			if step % round(lastTrackedPos.size()/6)==0:
-				step+=1
 		step += 1
-		if step-DELAY>=0:
-			if step-DELAY<lastTrackedPos.size():
-				nextMove = lastTrackedPos[step-DELAY]-position.x-32
+
+	
+
+
+func _on_replay_timer_timeout():
+	if step != -1:
+		replayStep += 1
+		if replayStep-delay>=0:
+			if replayStep-delay<lastTrackedPos.size():
+				nextMove = lastTrackedPos[replayStep-delay]-position.x-32
 			else :
 				nextMove = baseSpeed
-	
+		
